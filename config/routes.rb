5b4e610db62d72a48eb9a0aa6e656315e349653d
Sooyung Kim <kimx1279@gmail.com>
2023-10-15 21:09:35 -0600

    Add user story 22

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,20 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  post "/artists", to: "artists#create"
   get "/artists", to: "artists#index"
   get "/artists/new", to: "artists#new"
   get "/artists/:artist_id", to: "artists#show"
+  patch "/artists/:artist_id", to: 'artists#update'
+  delete "/artists/:artist_id", to: 'artists#destroy'
+
   get "/paintings", to: "paintings#index"
+  get "/paintings/:painting_id/edit", to: 'paintings#edit'
+  get "/paintings/:painting_id", to: "paintings#show"
+  patch "/paintings/:painting_id", to: 'paintings#update'
+  
   get "/artists/:artist_id/edit", to: "artists#edit"
   get "/artists/:artist_id/paintings/new", to: "artist_paintings#new"
   get "/artists/:artist_id/paintings", to: "artist_paintings#index"
   post "/artists/:artist_id/paintings", to: "artist_paintings#create"
-  post "/artists", to: "artists#create"
-  patch "/artists/:artist_id", to: 'artists#update'
-  get "/paintings/:painting_id/edit", to: 'paintings#edit'
-  get "/paintings/:painting_id", to: "paintings#show"
-  patch "/paintings/:painting_id", to: 'paintings#update'
-  delete "/artists/:artist_id", to: 'artists#destroy'
 end
